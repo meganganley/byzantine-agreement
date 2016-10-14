42e5ca19b7701b3a33e77e132b81b6b19a17edb9
Megan Ganley <mgn.gnly@gmail.com>
2016-10-14 22:46:41 +1300

    Working on eig tree

--- ByzantineAgreement/ByzantineAgreement/Program.cs
+++ ByzantineAgreement/ByzantineAgreement/Program.cs
@@ -11,6 +11,7 @@ namespace ByzantineAgreement
     class Program
     {
         private static int _n;
+        private static int _defaultValue;
 
         public static ActionBlock<Message>[] Byz;
         static int _completionCount;
@@ -22,7 +23,7 @@ namespace ByzantineAgreement
         {
             string[] temp = Console.ReadLine().Split(' ');
             _n = Convert.ToInt32(temp[0]);
-            int defaultValue = Convert.ToInt32(temp[1]);
+            _defaultValue = Convert.ToInt32(temp[1]);
 
             List<ByzNode> nodes = new List<ByzNode>();
             Byz = new ActionBlock<Message>[_n];
@@ -70,7 +71,14 @@ namespace ByzantineAgreement
 
         public class ByzNode
         {
-            public ByzNode() { }
+            public ByzNode()
+            {
+                MessagesReceived = 0;
+                RoundNumber = 1;
+
+                Root = new TreeNode(new Message(0, _defaultValue));
+              
+            }
 
             public ByzNode(int index, int initValue, bool faulty)
             {
@@ -79,11 +87,15 @@ namespace ByzantineAgreement
                 Faulty = faulty;
                 MessagesReceived = 0;
                 RoundNumber = 1;
+
+                Root = new TreeNode(new Message(0, _defaultValue));
+                
             }
             public int Index { get; set; }
             public int InitValue { get; set; }
             public bool Faulty { get; set; }
             public int[,] FaultyMessages { get; set; }
+            public TreeNode Root { get; set; }
 
             public int MessagesReceived;
             public int RoundNumber;
@@ -91,15 +103,30 @@ namespace ByzantineAgreement
             public void ByzBody(Message msg)
             {
 
-                Console.WriteLine("[{0}] {1} <- ({2}, {3})", tid(), Index, msg.From, msg.Value);
+                Console.WriteLine("[{0}] {1} <- ({2}, {3})", tid(), Index, msg.Node, msg.Value);
 
                 // indicates start of new round
-                if (msg.From == 0)
+                if (msg.Node == 0)
                 {
                     RoundNumber++;
+                    MessagesReceived = 0;
+
+
                     for (var i = 0; i < _n; i++)
                     {
-                        // send messages for corresponding round 
+                        TreeNode p = Root;
+                        TreeNode c = Root;
+
+                        // should find last completed level 
+                        while (p.Children.Count > 0)
+                        {
+                            c = p;
+                            p = p.Children.ElementAt(0);
+                        }
+
+
+                        // c.Children is messages to send 
+                        // send messages for corresponding round SOMEHOW 
 
                         if (Faulty)
                         {
@@ -107,6 +134,11 @@ namespace ByzantineAgreement
                         }
                         else
                         {
+                            foreach (TreeNode t in c.Children)
+                            {
+                                // value append 
+                                // index append 
+                            }
                             Byz[i].Post(new Message(Index, InitValue));
                         }
 
@@ -118,6 +150,33 @@ namespace ByzantineAgreement
                 {
                     MessagesReceived += 1;
 
+                    int index;
+                    TreeNode p = Root;
+                    TreeNode c;
+
+
+                    for ( int i = 0 ; i < GetNumberDigits(msg.Node); i ++)
+                    {
+                        index = GetDigit(msg.Node, i);
+
+
+
+                        c = p.Children.Find( x => x.Data.Node == index);
+
+                        // node doesnt exist yet, create
+                        if ( c == null)
+                        {
+                            p.Children.Add(new TreeNode(new Message(msg.Node, msg.Value)));
+                            break;
+                        }
+
+                        c = p;
+                    }
+
+             // add children at level round number 
+                //    Root.Children.Add(new Message());
+
+                    // TODO 
                     // add to some data structure 
 
                     if (MessagesReceived == _n && Interlocked.Decrement(ref _completionCount) == 0)
@@ -127,6 +186,7 @@ namespace ByzantineAgreement
                 }
             }
 
+            // debug
             public override string ToString()
             {
                 if (Faulty)
@@ -148,11 +208,27 @@ namespace ByzantineAgreement
 
         public class Message
         {
-            public Message(int from, int value) { From = from; Value = value; }
-            public int From { get; private set; }
+            public Message(int node, int value) { Node = node; Value = value; }
+
+            // Node is the full path of information 
+            public int Node { get; private set; }
             public int Value { get; private set; }
+
+            public override string ToString()
+            {
+                return Node + " : " + Value;
+            }
         }
 
+        private static int GetDigit(int num, int digit)
+        {
+            return Convert.ToInt32(num.ToString().Substring(digit, digit + 1));
+        }
+
+        private static int GetNumberDigits(int num)
+        {
+            return num.ToString().Length;
+        }
         
         /* HELPER METHODS */
         private static ByzNode NodeFromInput(string s)
@@ -199,6 +275,41 @@ namespace ByzantineAgreement
             return ret;
         }
 
+        public static int CreateMessage()
+        {
+            return 0; 
+        }
+
+        public class TreeNode
+        {
+            //public List<TreeNode> _childrenAtLevel;
+            public TreeNode Parent { get; set; }
+            public List<TreeNode> Children { get; set; }
+
+//            public List<TreeNode> ChildrenAtLevel
+//            {
+//                get { return _childrenAtLevel; }
+//                set { _childrenAtLevel = value; }
+//            }
+
+
+            public Message Data { get; set; }
+            public int Consensus { get; set; }
+
+            // maybe level? count of children?
+
+            public TreeNode(Message data)
+            {
+                Data = data;
+                Children = new List<TreeNode>();
+            }
+
+
+            public override string ToString()
+            {
+                return Data.ToString();
+            }
+        }
 
     }
 
